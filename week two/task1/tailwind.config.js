af10c8923b89717f70f6c065c42b124f921143d1
Yahia-Ahmed-abdelrazik <otakayahia@gmail.com>
2024-11-07 13:31:39 +0200

    week-two-task1-nav

---
+++ week two/task1/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1F1F1F",
+        secondary: "#545454",
+      },
+    },
+  },
+  plugins: [],
+};
