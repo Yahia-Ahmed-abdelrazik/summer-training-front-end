8e31eb4fe1236db7bcbfb4214b456eb1e77d9e1d
Yahia-Ahmed-abdelrazik <otakayahia@gmail.com>
2024-11-12 21:46:53 +0200

    add footer

--- week two/task1/tailwind.config.js
+++ week two/task1/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors: {
         primary: "#1F1F1F",
         secondary: "#545454",
+        third: "#D1D1D1",
       },
       fontFamily: {
         sans: ["Inter", "sans-serif"], // Add 'Inter' as the default sans font
